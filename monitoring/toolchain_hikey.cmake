c0156bb2a552e2ce6b3da1631c7274041eee8963
Martin Rapp <ma.rapp@gmx.de>
2020-10-07 11:35:48 +0200

    typo

--- monitoring/toolchain_hikey.cmake
+++ monitoring/toolchain_hikey.cmake
@@ -2,4 +2,4 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # Specify the cross compiler
-SET(CMAKE_C_COMPILER /pyth/to/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
+SET(CMAKE_C_COMPILER /path/to/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
