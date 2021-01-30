725e29679e8ef64e296b51f5def41be721f87d44
Nikita K <upupq@student.kit.edu>
2021-01-30 15:52:03 +0100

    initial commit

---
+++ benchmarks/parsec/build_and_push.sh
@@ -0,0 +1,12 @@
+adb push binaries  /data/ces/parsec
+adb shell chmod 0111 /data/ces/parsec/blackscholes/blackscholes
+adb shell chmod 0111 /data/ces/parsec/bodytrack/bodytrack
+adb shell chmod 0111 /data/ces/parsec/canneal/canneal
+adb shell chmod 0111 /data/ces/parsec/dedup/dedup
+adb shell chmod 0111 /data/ces/parsec/facesim/facesim
+adb shell mkdir -p /data/ces/parsec/facesim/Storytelling/output
+adb shell chmod 0111 /data/ces/parsec/ferret/ferret
+adb shell chmod 0111 /data/ces/parsec/fluidanimate/fluidanimate
+adb shell chmod 0111 /data/ces/parsec/freqmine/freqmine
+adb shell chmod 0111 /data/ces/parsec/swaptions/swaptions
+adb shell chmod 0111 /data/ces/parsec/x264/x264
--- monitoring/toolchain_hikey.cmake
+++ monitoring/toolchain_hikey.cmake
@@ -2,4 +2,5 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # Specify the cross compiler
-SET(CMAKE_C_COMPILER /pyth/to/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
+SET(CMAKE_C_COMPILER /home/nikita/Documents/Work/hikey970/benchmarks/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER /home/nikita/Documents/Work/hikey970/benchmarks/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-g++)
